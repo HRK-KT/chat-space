2c75e4285f4c0cdb56dc1fa96dd65ddf229f8440
HRK-KY <humansequencer0@gmail.com>
2020-07-30 21:47:40 +0900

    able to send in succession

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -53,6 +53,7 @@ $(function(){
       $('.Chat-main__message-list').append(html);
       $('.Chat-main__message-list').animate({ scrollTop: $('.Chat-main__message-list')[0].scrollHeight});
       $('form')[0].reset();
+      $('.Form__sendBtn').removeAttr("disabled");
     })
   });
 });
