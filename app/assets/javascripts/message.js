f5da7d7fb59b2917a2abb5f4f11fe229cf1435f5
HRK-KY <humansequencer0@gmail.com>
2020-07-30 21:20:12 +0900

    create HTML after accepted JSON by done method

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,4 +1,45 @@
 $(function(){
+  function buildHTML(message){
+    if ( message.image ) {
+      let html = 
+        `<div class="Chat-main__message-list">
+          <div class="Message-info">
+            <div class="Message-info__user">
+              ${message.user_name}
+            </div>
+            <div class="Message-info__date">
+              ${message.created_at}
+            </div>
+          </div>
+          <div class="Message-body">
+            <p class="Message-content">
+              ${message.body}
+            </p>
+            <img class="Message-image" src="${message.image}">
+          </div>
+        </div>`
+        return html;
+  } else {
+      let html =
+        `<div class="Chat-main__message-list">
+          <div class="Message-info">
+            <div class="Message-info__user">
+              ${message.user_name}
+            </div>
+            <div class="Message-info__date">
+              ${message.created_at}
+            </div>
+          </div>
+          <div class="Message-body">
+            <p class="Message-content">
+              ${message.body}
+            </p>
+          </div>
+        </div>`
+        return html;
+  };
+}
+
   $('.Form').on('submit', function(e){
     e.preventDefault();
     let formData = new FormData(this);
@@ -10,6 +51,9 @@ $(function(){
       dataType: 'json',
       processData: false,
       contentType: false
+    })
+    .done(function(data){
+      let html = buildHTML(data);
     })
   });
 });
